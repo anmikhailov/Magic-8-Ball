f04c92e34f55a1e091a23409de4464d286ad7a76
Andrey Mikhailov <mands315@gmail.com>
2023-03-30 10:08:40 +0300

    Add Ask button pressed action

--- Magic 8 Ball/ViewController.swift
+++ Magic 8 Ball/ViewController.swift
@@ -9,11 +9,22 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
+	//MARK: - Outlets
+	@IBOutlet weak var magicBall: UIImageView!
 
+	//MARK: - Actions
+	@IBAction func askButtonPressed(_ sender: UIButton) {
+		
+		let answerBallsArray = [
+			UIImage(named: "ball1")
+			UIImage(named: "ball2")
+			UIImage(named: "ball3")
+			UIImage(named: "ball4")
+			UIImage(named: "ball5")
+		]
+		
+		magicBall.image = answerBallsArray.randomElement() as? UIImage
+	}
 
 }
 
