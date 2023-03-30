7cb945ad4a7fea6d5350c5f1d4d565f6eb0e89e7
Andrey <mands315@gmail.com>
2023-03-30 21:38:02 +0300

    Add assets. Add magicBall image and Ask button

--- Magic 8 Ball/ViewController.swift
+++ Magic 8 Ball/ViewController.swift
@@ -12,19 +12,19 @@ class ViewController: UIViewController {
 	//MARK: - Outlets
 	@IBOutlet weak var magicBall: UIImageView!
 
+    
 	//MARK: - Actions
 	@IBAction func askButtonPressed(_ sender: UIButton) {
 		
 		let answerBallsArray = [
-			UIImage(named: "ball1")
-			UIImage(named: "ball2")
-			UIImage(named: "ball3")
-			UIImage(named: "ball4")
+			UIImage(named: "ball1"),
+			UIImage(named: "ball2"),
+			UIImage(named: "ball3"),
+			UIImage(named: "ball4"),
 			UIImage(named: "ball5")
 		]
 		
 		magicBall.image = answerBallsArray.randomElement() as? UIImage
 	}
-
 }
 
